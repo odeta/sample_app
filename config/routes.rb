4b79c2415d519be59167d3690f48a8bfa705d375
Odeta <odeta.kizyte@gmail.com>
2018-04-26 22:32:37 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # creates helper root_url
+  root 'static_pages#home'
+  # maps requests for the URL /static_pages/home
+  # to the home action in the Static Pages controller
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  root 'application#hello'
+  # automatically creates a helper called static_pages_about_url
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
